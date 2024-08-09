516cee14da36dd37219006c91a237589cdeefbef
Aelkoulali <abdellah.elkoulali@gmx.fr>
2024-08-09 13:00:37 +0200

    Add Eventlistener() to sortButton

--- script.js
+++ script.js
@@ -1,2 +1,5 @@
 // Access to the id from the html document
 const sortButton = document.getElementById("sort");
+
+// Add Eventlistener() to sortButton
+sortButton.addEventListener("click", sortInputArray);
