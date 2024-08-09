d9d21f1b9d80ce2259bbf0171f592fc4272d26dc
Aelkoulali <abdellah.elkoulali@gmx.fr>
2024-08-09 13:02:03 +0200

    Add sortInputArray function

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
 // Access to the id from the html document
 const sortButton = document.getElementById("sort");
 
+
+// Add sortInputArray function 
+const sortInputArray = (event) => {
+    event.preventDefault();
+  
+    const inputValues = document.getElementsByClassName("values-dropdown");
+  }
+
+
 // Add Eventlistener() to sortButton
 sortButton.addEventListener("click", sortInputArray);
+
