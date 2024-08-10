c0402778903c9d63e068a26884257ef3274dda0b
Aelkoulali <abdellah.elkoulali@gmx.fr>
2024-08-10 20:47:37 +0200

    Use map( ) on inputValues array

--- script.js
+++ script.js
@@ -6,7 +6,8 @@ const sortButton = document.getElementById("sort");
 const sortInputArray = (event) => {
     event.preventDefault();
   
-    const inputValues =[...document.getElementsByClassName("values-dropdown")];
+  event.preventDefault();
+  const inputValues =[...document.getElementsByClassName("values-dropdown")].map((dropdown) => dropdown.value);
   }
 
 
