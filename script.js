39c6bbde133d3f8b38ede8a21a7e28185e70809d
Aelkoulali <abdellah.elkoulali@gmx.fr>
2024-08-12 13:39:54 +0200

    Add if statement

--- script.js
+++ script.js
@@ -26,6 +26,12 @@ const sortInputArray = (event) => {
  const bubbleSort = (array) => {
   for(let i = 0; i < array.length; i++){
     for(let j = 0; j < array.length - 1; j++){
+      if (array[j] > array[j + 1]) {
+        const temp = array[j];
+        array[j] = array[j + 1];
+        array[j + 1] = temp.value;
+
+      }
 
     }
 
